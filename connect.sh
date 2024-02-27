24a4514ed8a29ff9cb87bd11e6e943a91f302f27
W1412X <tor108@outlook.com>
2024-02-27 23:45:45 +0800

    a new repository

--- connect.sh
+++ connect.sh
@@ -4,4 +4,4 @@ sudo apt update
 echo "|-> Install the openssh-server"
 sudo apt install openssh-server -y
 echo "|-> Config the file /etc/ssh/sshd_config"
-sudo sed -i 's/^#\(.*Port 20.*\)/\1/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\(.*Port 22.*\)/\1/' /etc/ssh/sshd_config
